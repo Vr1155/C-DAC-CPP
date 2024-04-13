37104701d2b5780a7c0d03e7c93ba445eb028f43
Vr1155 <49389720+Vr1155@users.noreply.github.com>
2024-04-13 13:02:19 +0530

    Added day 3 code and notes for makefiles and make variables. (#48)

---
+++ parallel/d3/makeBuildAutomation/programTwo.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+// declarations:
+int x;
+void fun();
---
+++ parallel/d3/morebuildAutomation/program2Main.c
@@ -0,0 +1,13 @@
+#include <programTwo.h>
+
+
+int main(){
+	printf("Printing... %d\n", x);
+	printf("Running the fun() here:\n");
+	fun();
+	printf("Running the printfun() here:\n");
+	printfun();
+	printf("Exiting program!\n");
+	exit(0);
+}
+
